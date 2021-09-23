5d3726290c393049deb81169279bfea348b46c61
MauroCG <mauro57.mauricio@gmail.com>
2021-09-23 12:08:46 -0500

    Exercise 1.3 solved

--- part1/exercise_course_information/src/App.js
+++ part1/exercise_course_information/src/App.js
@@ -44,14 +44,20 @@ const Total = ({exercises}) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-  const lessons = [part1, part2, part3]
-  const contents = [exercises1, exercises2, exercises3]
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+  const lessons = [part1.name, part2.name, part3.name]
+  const contents = [part1.exercises, part2.exercises, part3.exercises]
 
   return (
     <>
@@ -60,7 +66,7 @@ const App = () => {
         lessons={lessons}
         contents={contents}  
       />
-      <Total exercises={[exercises1, exercises2, exercises3]} />
+      <Total exercises={contents} />
     </>
   );
 }
