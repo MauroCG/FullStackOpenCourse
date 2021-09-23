ba2164d685db8cb55d9f707c3a3fb7c46627b30c
MauroCG <mauro57.mauricio@gmail.com>
2021-09-23 15:40:51 -0500

    Start part1 section c. Component state, helper function

--- part1/course_content/src/App.js
+++ part1/course_content/src/App.js
@@ -1,30 +1,25 @@
-const Hello = (props) => {  
-  return (    
-    <div>
-      <p>Hello {props.name}</p>
-    </div>
-    )
-  };
-
-
-  const Footer = () => {
-    return (
-      <div>
-        greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
-      </div>
-    )
-  }
-
-
-  const App = () => {
+const Hello = (props) => {
+  const bornYear = () => {    const yearNow = new Date().getFullYear()    return yearNow - props.age  }
   return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name="George" />
-      <Hello name="Daisy" />
-      <Footer />
-    </>
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+      <p>So you were probably born in {bornYear()}</p>    </div>
   )
-};
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  )
+}
 
 export default App;
