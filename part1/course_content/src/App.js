f26db3d2c787e25bfddeefdc0e7e2436eb7fdc17
MauroCG <mauro57.mauricio@gmail.com>
2021-09-25 12:43:29 -0500

    Part1 course content, section d. Functions returning functions.

--- part1/course_content/src/App.js
+++ part1/course_content/src/App.js
@@ -1,44 +1,18 @@
 import React, { useState } from "react";
 
-const History = (props) => {  
-  if (props.allClicks.length === 0) {    
-    return (      
-      <div>the app is used by pressing the buttons</div>    
-      )  
-    }  
-  return (    
-    <div>      
-      button press history: {props.allClicks.join(' ')}    
-    </div>  
-  )
-}
-
-const Button = ({ handleClick, text }) => (  
-  <button onClick={handleClick}>    
-    {text}  
-  </button>
-)
-
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-  const handleLeftClick = () => {    
-    setAll(allClicks.concat('L'))    
-    setLeft(left + 1)  
-  }
-  const handleRightClick = () => {    
-    setAll(allClicks.concat('R'))    
-    setRight(right + 1)  
+  const [value, setValue] = useState(10)
+
+  const hello = (who) => () => {
+    console.log('hello', who)
   }
 
   return (
     <div>
-      {left}
-      <Button handleClick={handleLeftClick} text='left' />
-      <Button handleClick={handleRightClick} text='right' />
-      {right}
-      <History allClicks={allClicks} />    
+      {value}
+      <button onClick={hello('world')}>button</button>
+      <button onClick={hello('react')}>button</button>
+      <button onClick={hello('function')}>button</button>
     </div>
   )
 }
