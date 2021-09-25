7229d22e4ff945525a1b318293b29ee9a12e83ec
MauroCG <mauro57.mauricio@gmail.com>
2021-09-25 14:37:41 -0500

    Starting part1 exercise unicafe

---
+++ part1/exercise_unicafe/README.md
@@ -0,0 +1,3 @@
+# Solutions to the part1 unicafe exercises
+
+This project ocntain the solution to the **exercises from 1.6 to 1.11**.
---
+++ part1/exercise_unicafe/src/App.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+
+function App() {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0);
+
+  return (
+    <>
+      <h2>give feedback</h2>
+    </>
+  );
+}
+
+export default App;
