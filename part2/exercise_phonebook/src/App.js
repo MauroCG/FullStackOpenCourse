8cd45fa337908b6acd14013bfd24d9992c4d3e6e
MauroCG <mauro57.mauricio@gmail.com>
2021-09-28 19:13:41 -0500

    Exercise 2.8 solved

--- part2/exercise_phonebook/src/App.js
+++ part2/exercise_phonebook/src/App.js
@@ -2,27 +2,39 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const handleChangeNewName = (event) => {
     setNewName(event.target.value)
     //console.log(event.target.value)
   }
 
+  const handleChangeNewNumber = (event) => {
+    setNewNumber(event.target.value)
+  }
+
   const addPerson = (event) => {
     event.preventDefault()
     const newPerson = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
 
-    const alreadyAdded = persons.some(person => person.name === newPerson.name)
-    if (alreadyAdded) {
+    const alreadyAddedName = persons.some(person => person.name === newPerson.name)
+    const alreadyAddedNumber = persons.some(person => person.number === newPerson.number)
+
+    if (alreadyAddedName) {
       alert(`${newPerson.name} is already added to phonebook`)
-    } else {
+    } else if (alreadyAddedNumber) {
+      alert(`${newPerson.number} is already added to phonebook`)
+    }
+    else {
       setPersons(persons.concat(newPerson))
       setNewName('')
+      setNewNumber('')
     }
   }
 
@@ -34,12 +46,17 @@ const App = () => {
           name: <input value={newName} onChange={handleChangeNewName} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleChangeNewNumber} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
         {persons.map(person =>
-          <div key={person.name}>{person.name}</div>
+          <div key={person.name}>
+          {person.name} {person.number}
+        </div>
         )}
     </div>
   )
