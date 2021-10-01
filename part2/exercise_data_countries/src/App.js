35ee34fe2b7a037d6b7747aa17a9ef3514ed8b07
MauroCG <mauro57.mauricio@gmail.com>
2021-09-30 23:07:54 -0500

    Created the account to get the api key. The app fetches at init all countries data

--- part2/exercise_data_countries/src/App.js
+++ part2/exercise_data_countries/src/App.js
@@ -1,10 +1,26 @@
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
+  // States for countries
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    const API_KEY = process.env.REACT_APP_COUNTRIES_API
+    axios
+      .get(`http://api.countrylayer.com/v2/all?access_key=${API_KEY}`)
+      .then(response => setCountries(response.data))
+      .catch(error => console.log(error))
+  }, [])
+
   return (
-    <div>
-      <p>Data for countries exercise from the fullstackopen course of Helsinky University.</p>
-    </div>
+    <>
+      <div>
+        Find countries (by name): <input type="search" />
+      </div>
+      <hr />
+      {countries.map(country => <div key={country.alpha3Code}>{country.name}</div>)}
+    </>
   );
 }
 
