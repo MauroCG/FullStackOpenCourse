280418a1aaf83ba1705cd45a0cc24d33e126637c
MauroCG <mauro57.mauricio@gmail.com>
2021-10-22 14:32:43 -0500

    Exercise 3.14 solved (added functionality for get one by id and delete too).

--- part3/exercise_phonebook_backend/index.js
+++ part3/exercise_phonebook_backend/index.js
@@ -27,41 +27,6 @@ const customLogging = (tokens, req, res) => {
 }
 app.use(morgan(customLogging))
 
-let persons = [ // Hardcoded data
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
-
-const generateId = () => {
-    let id = Math.floor(Math.random() * 10000) // Creates a new random id
-
-    // Verify that the id doesn't match any one
-    ids = persons.map(p => p.id)
-    while (id in ids) {
-        id = Math.floor(Math.random() * 10000)
-    }
-
-    return id
-}
 
 
 app.get('/api/persons', (request, response) => { // Get all the data
@@ -70,71 +35,48 @@ app.get('/api/persons', (request, response) => { // Get all the data
     })
 });
 
-app.get('/info', (request, response) => { // Get info of the phonebook
-    const current_time = new Date()
-    response.send(`
-        Phonebook has info for ${persons.length} people
-        <br></br>
-        ${current_time}
-    `)
-})
-
 app.get('/api/persons/:id', (request, response) => { // Geit information of a single person
-    const id = Number(request.params.id)
-    const person = persons.find(p => p.id === id)
-
-    if (person) {
-        response.json(person)
-    } else {
-        response.statusMessage = `The id ${id} hasn't found for any person in the phonebook`
+    Person.findById({ _id: request.params.id }).then(result => {
+        response.json(result)
+    })
+    .catch(error => {
+        response.statusMessage = "Person hasn't found in the phonebook"
         response.status(404).end()
-    }
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => { // Deletes the information of a single person
-    const id = Number(request.params.id)
-    //console.log(id, persons.map(p => p.id).includes(id))
-
-    if (persons.map(p => p.id).includes(id)) {
-        //console.log(`Deleting the person with the id ${id}`)
-        persons = persons.filter(p => p.id !== id)
-        response.statusMessage = `The person with id ${id} was deleted successfully`
-    } else {
-        response.statusMessage = `The id ${id} hasn't found for any person in the phonebook`
-    }
-
-    response.status(204).end()
-
+    Person.findByIdAndDelete({ _id: request.params.id }).then(result => {
+        console.log(result)
+        response.status(204).end()
+    })
 })
 
 app.post('/api/persons', (request, response) => { // Saves information of a single person to the phonebook
-    const newId = generateId() // Generates a new id
-
     if (!request.body.name || !request.body.number) { // Error handling for missing information
         return response.status(400).json({
             error: "Name or number are missing, please supply required information"
         })
     }
 
-    if (persons.map(p => p.name).includes(request.body.name)) { // Error handling for given a repeated name
-        return response.status(400).json({
-            error: "The name must be unique"
-        })
-    }
-    /*persons.map(p => {
-        console.log(p.name, typeof p.name, typeof request.body.name)
-    })*/
+    Person.find({ name: request.body.name }).then(result => { // Error handling for given a repeated name
+        if (result.length > 0) {
+            return response.status(400).json({
+                error: 'The name mut be unique'
+            })
+        }
+    })
 
-    const person = { // Creates a new person
-        id: newId,
+    console.log('Creating the person')
+    const person = Person({ // Creates a new person
         name: request.body.name,
         number: request.body.number
-    }
-    //console.log(person)
-    persons = persons.concat(person) // Adds the new person to the phonebook
+    })
 
-
-    response.json(person)
+    console.log('Saving the person to the DB')
+    person.save().then(addedPerson => {
+        response.json(addedPerson)
+    })
 })
 
 
