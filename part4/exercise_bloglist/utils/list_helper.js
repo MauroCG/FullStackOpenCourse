cb31061b52eb041c0dcaa4a8b89d04e520396c3d
MauroCG <mauro57.mauricio@gmail.com>
2021-11-03 17:23:55 -0500

    Exercise 4.5 solved.

--- part4/exercise_bloglist/utils/list_helper.js
+++ part4/exercise_bloglist/utils/list_helper.js
@@ -14,8 +14,29 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return 0
+  }
+
+  let favorite = blogs[0]
+
+  blogs.forEach(blog => {
+    if (blog.likes > favorite.likes) {
+      favorite = blog
+    }
+  })
+
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes
+  }
+}
+
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
